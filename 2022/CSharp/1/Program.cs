806dbc6f24b27bcc93445e3ff40db120deaa0465
c-kirkeby <christian@christiankirkeby.com>
2024-12-02 08:54:21 +1000

    initial

---
+++ 2022/CSharp/1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// string? line;
+// List<int> elves = new List<int>();
+// List<int> calories = new List<int>();
+// // int index = 0;
+// while ((line = Console.ReadLine()) is not null) {
+//   if (line == "") {
+//     calories.Add(null)
+//   }
+//   calories.Add(int.Parse(line));
+// };
+//
+string? line;
+
+// List<List<int>> elves = new List<List<int>>();
+List<int> elves = new List<int>();
+
+int elf = 0;
+
+while ((line = Console.ReadLine()) is not null) {
+  if (line != "") {
+    elves[elf] = elves[elf] + int.Parse(line);
+  } else {
+    elves.Add(0);
+    elf++;
+  }
+}
+
+// foreach (int elf in elves) {
+//   Console.WriteLine(elf.ToString());
+// }
---
+++ 2023/CSharp/Dec 1/Program.cs
@@ -0,0 +1,26 @@
+﻿int GetCalibrationValue(string line)
+{
+
+    List<char> numbers = line.Where(char.IsNumber).ToList();
+
+    return int.Parse(numbers.First().ToString() + numbers.Last());
+}
+
+try
+{
+    StreamReader streamReader = new StreamReader("calibration.txt");
+    string? line = streamReader.ReadLine();
+    int sum = 0;
+
+    while (line != null)
+    {
+        sum += GetCalibrationValue(line);
+        line = streamReader.ReadLine();
+    }
+
+    Console.WriteLine($"Calibration value: {sum}");
+}
+catch (Exception exception)
+{
+    Console.WriteLine($"Error reading file: {exception.Message}");
+}
